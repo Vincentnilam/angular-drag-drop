dcdd20b7180d43da72badfbd9851535bdace439a
Vincent Nilam <vincent.nilam@hotmail.com>
2025-09-08 00:39:15 +1000

    feat: add drag and drop poc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CdkDrag, CdkDropList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-drag-drop');
+
+  players = [
+    'Cliven',
+    'Darren',
+    'Vincent',
+  ];
+
+  sourcePlayers = ['Toby', 'Michelle'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+    
+  }
 }
